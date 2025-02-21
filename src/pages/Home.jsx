dadf65cccafaa489869b2055d069ec5f401ebc16
Shoaibi7 <m.shoaib2k15@gmail.com>
2025-02-21 19:03:02 +0500

    Added reactbit library

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 // src/pages/Home.jsx
 import React from "react";
-
+import Hyperspeed from "../blocks/Backgrounds/Hyperspeed/Hyperspeed";
+import Aurora from "../blocks/Backgrounds/Aurora/Aurora";
 function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to the Redux Toolkit App!</h1>
+      <h1 className="bg-red-500 p-4">Welcome to the Redux Toolkit App!</h1>
       <p>This is the home page.</p>
+
+      <Aurora />
     </div>
   );
 }
